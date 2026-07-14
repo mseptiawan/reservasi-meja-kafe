762427b610e8b59886529d86ad563cf92e2f44a6
mseptiawan <mseptiawan017@gmail.com>
2026-07-14 10:56:07 +0700

    feat: tambah landing page

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['reservation_id', 'payment_code', 'amount', 'proof_of_payment', 'status', 'admin_note'])]
 class Payment extends Model
 {
-    //
+    use HasFactory;
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
