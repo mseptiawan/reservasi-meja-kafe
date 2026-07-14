4a046daebd72555ea3e83ab8b1261be1677a612b
mseptiawan <mseptiawan017@gmail.com>
2026-07-14 09:50:47 +0700

    feat: add model n migration Table

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// Route::middleware('auth')->group(function () {
+//     Route::get('/table', )
+// })
 require __DIR__.'/auth.php';
